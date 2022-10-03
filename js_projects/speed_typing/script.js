2ac7e0900a6abdff5486b5298f27a889fbd8a3ae
waqas <105483652+waqas-on-github@users.noreply.github.com>
2022-10-03 06:49:22 +0500

    minor changes

--- js_projects/speed_typing/script.js
+++ js_projects/speed_typing/script.js
@@ -1,10 +1,11 @@
-let show_qoute =document.querySelector(".quote-display")
-let text_area =document.querySelector("#quoteInput")
+
+let show_text =document.querySelector(".quote-display")
+let input_text =document.querySelector("#quoteInput")
 
 
 // for removing text here after few seconds of page load  
 setTimeout(()=>{
-   text_area.innerHTML=""
+   input_text.innerHTML=""
 } ,1000) 
 
 
@@ -15,23 +16,27 @@ async function Get_reandom_qoute(){
     content.content.split('').forEach(element => {
          let ch_span =document.createElement("span")
          ch_span.innerText=element
-         show_qoute.appendChild(ch_span)
+         show_text.appendChild(ch_span)
       });
       
       }
 Get_reandom_qoute()
     
-text_area.addEventListener("input",() => {
-   let array_value =text_area.value.split('')
+input_text.addEventListener("input",() => {
+
+   let input_text_value =input_text.value.split('')
    let elements =document.querySelectorAll("span")
+
    elements.forEach((elem,index) => {
-    let character=array_value[index]
-    console.log(elem.innerHTML,index,character)
-     
+    let character=input_text_value[index]
+    
     if (character == null) {
+        
         elem.classList.remove('correct')
         elem.classList.remove('incorrect')
       } else if (character === elem.innerText) {
+
+        
         elem.classList.add('correct')
         elem.classList.remove('incorrect')
       } else {

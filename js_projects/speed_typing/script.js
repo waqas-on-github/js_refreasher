17ee8377783a9d262d8b9e97b0df46e00c1031ae
waqas <105483652+waqas-on-github@users.noreply.github.com>
2022-10-09 02:20:48 +0500

    time feature added

--- js_projects/speed_typing/script.js
+++ js_projects/speed_typing/script.js
@@ -2,12 +2,20 @@
 let show_text =document.querySelector(".quote-display")
 let input_text =document.querySelector("#quoteInput")
 
-
 // for removing text here after few seconds of page load  
+
 setTimeout(()=>{
    input_text.innerHTML=""
 } ,1000) 
+// timer code 
+let time=document.querySelector(".timer")
+console.log(time.innerText)
 
+
+setInterval(() => {
+  time.innerText++
+  console.log(time.innerText)
+}, 1000);
 // get data from api and show on page 
 
 async function Get_reandom_qoute(){
@@ -15,17 +23,18 @@ async function Get_reandom_qoute(){
     const  data = await fetch("https://api.quotable.io/random")
     let content = await data.json()
     content.content.split('').forEach(element => {
-         let ch_span =document.createElement("span")
+    let ch_span =document.createElement("span")
          ch_span.innerText=element
          show_text.appendChild(ch_span)
-      });
-      
-      }
+      })}
+
 Get_reandom_qoute()
-    
+
 // oninput  event compare input word and do decisions 
 
 input_text.addEventListener("input",() => {
+   let correct=true  // when we input value will be true 
+   
 
    let input_text_value =input_text.value.split('')
    let elements =document.querySelectorAll("span")
@@ -37,16 +46,32 @@ input_text.addEventListener("input",() => {
         
         elem.classList.remove('correct')
         elem.classList.remove('incorrect')
-      } else if (character === elem.innerText) {
-
-        
+        correct=false     // if an word false mean all phara false 
+      } 
+      else if (character === elem.innerText) {
         elem.classList.add('correct')
         elem.classList.remove('incorrect')
+        
       } else {
         elem.classList.remove('correct')
         elem.classList.add('incorrect')
+        correct=false //if an word false mean all phara false 
       }
-   });
-
-
+   }); 
+   // all conditions checked there will must be all values be true so load another phara 
+   
+if(correct){load_qoute()}
+                 
 })
+               
+               
+               
+function load_qoute(){
+   setTimeout(() => {
+   Get_reandom_qoute()
+   show_text.innerText=''
+   input_text.value=""
+   time.innerText=0
+    }, 2000);
+
+ }
